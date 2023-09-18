5eba4b9696730446f2ab61ab097dff03a6694d6c
admin <wertua17062004@gmail.com>
2023-09-18 19:13:08 +0300

    Реалізував завдання з 1 лабораторної роботи

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
         try {
             CalculatingTask1 calculatingTask1 = new CalculatingTask1();
 
-            System.out.println(calculatingTask1.calcMethod(3, 4, 9, 10));
+            System.out.println(calculatingTask1.calcMethod(3, 4, 5, 6));
         } catch (IllegalArgumentException e) {
             System.out.println("Перевірте коректність введених даних " +  e.getMessage());
         }
